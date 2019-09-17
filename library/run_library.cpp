dadbda51cf0a6ed4603582c0b7c61d8cbf0babf6
Felix Muzny <muzny@colorado.edu>
2019-09-17 12:41:24 -0600

    Library files that are ready to go

---
+++ library/run_library.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include "Library.h"
+
+/* 
+File comment should go here
+
+*/
+
+
+int main() {
+
+    // start off with one library instantiated
+    Library lib;
+
+}
