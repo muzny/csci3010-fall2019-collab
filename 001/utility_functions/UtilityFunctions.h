2bed0571712fb920938ec8055025e29aae4e1666
mauraw <mauraw@MacBook-Pro-2.local>
2019-09-22 14:19:54 -0600

    maura

--- 001/utility_functions/UtilityFunctions.h
+++ 001/utility_functions/UtilityFunctions.h
@@ -1,2 +1,4 @@
 // put the declarations of your utility functions here.
+#include <iostream>
+std::vector<std::string> Split(std::string whole, std::string separator);
 // they should not be in a class.
