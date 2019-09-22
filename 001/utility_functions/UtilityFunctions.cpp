905ba849b38dd3031e453cde63f0ccce12c760f0
Oceane Andreis <oceanestars13@gmail.com>
2019-09-22 17:58:03 -0600

    Sum function

--- 001/utility_functions/UtilityFunctions.cpp
+++ 001/utility_functions/UtilityFunctions.cpp
@@ -1,3 +1,16 @@
 #include "UtilityFunctions.h"
 
 // Implement your functions here. Don't forget function comments!
+
+int Sum(std::vector<int> nums){
+     
+    int sum = 0;
+    
+    for(int i =0; i<nums.size(); i++)
+    {
+        sum+=nums[i];
+    }
+    return sum;
+
+    
+}
