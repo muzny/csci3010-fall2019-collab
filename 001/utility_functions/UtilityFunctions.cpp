261c34c419373ee1824dd21b660b50db3f58424c
Kamiar Coffey <kamiar.coffey@gmail.com>
2019-09-23 12:58:11 -0600

    lec8

--- 001/utility_functions/UtilityFunctions.cpp
+++ 001/utility_functions/UtilityFunctions.cpp
@@ -1,3 +1,11 @@
 #include "UtilityFunctions.h"
 
-// Implement your functions here. Don't forget function comments!
+// adds n to each element of the vector
+// Kamiar Coffey
+std::vector<double> AddN(std::vector<double> input_vector, double n) {
+  std::vector<double> return_vector;
+  for (double number : input_vector) {
+    return_vector.push_back(number + n);
+  }
+  return return_vector;
+}
--- 001/utility_functions/main.cpp
+++ 001/utility_functions/main.cpp
@@ -1,10 +1,22 @@
 #include "UtilityFunctions.h"
+#include <iostream>
 
+// Compile instructions
+// run $ make all
+// run $ ./test
+// unit tests are NOT in main -- they are in test.cpp
+// unit tests will be run automatically when compling via Makefile
 
 int main() {
-    // the main function should have demonstrations
-    // of running your functions.
-    // make sure that the output is easily interpretable.
-    // If you'd like to define helper functions in this file,
-    // you are welcome to do so!
+  std::vector<double> original = {0,1,2};
+  for (double d : original) {
+    std::cout << d << " ";
+  }
+  std::cout << std::endl << "AddN vector \n";
+
+  std::vector<double> updated = AddN(original, 2);
+  for (double d : updated) {
+    std::cout << d << " ";
+  }
+  std::cout << std::endl;
 }
