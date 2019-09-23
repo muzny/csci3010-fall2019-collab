3a938f2e9ae77e553745dc20b8ba9732458d684a
pickettmap <43684405+pickettmap@users.noreply.github.com>
2019-09-22 18:05:04 -0600

    Adding test cases for intsum

--- 001/utility_functions/test.cpp
+++ 001/utility_functions/test.cpp
@@ -6,3 +6,24 @@
 // Your tests go here
 // Each TEST_CASE should test one function
 // Each SECTION should test one aspect of that function
+TEST_CASE("Test Addition", "[vector]")
+{
+  SECTION("Test with different numbers") {
+      REQUIRE(Sum(nums) == "20"); 
+      REQUIRE(Sum(nums2) == "19");
+      REQUIRE(Sum(nums3) == "210");
+    
+  }
+ 
+  SECTION("Test add in a new number and then add") {
+      nums3.push_back(10);
+      REQUIRE(Sum(nums2) == "220");
+      
+  }
+    
+  SECTION("Test add a negative number and then add") {
+      nums3.push_back(-10);
+      REQUIRE(Sum(nums2) == "200");
+      
+  }
+}
