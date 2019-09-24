fb2920f3f04e55b146706aa29d8396df4087a3fa
LucasKolanz <luko8622@colorado.edu>
2019-09-23 19:25:10 -0600

    Added factorial function4

--- 001/utility_functions/UtilityFunctions.cpp
+++ 001/utility_functions/UtilityFunctions.cpp
@@ -1,3 +1,13 @@
 #include "UtilityFunctions.h"
 
 // Implement your functions here. Don't forget function comments!
+
+int Factorial(int n)
+{
+	int returnThis = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		returnThis*=i;
+	}
+	return returnThis;
+}
