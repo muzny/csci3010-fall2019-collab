c72506bb2bc8d0ec7a77c3b150c7cc52e9d6e7d6
MugBergerFries <sjamesberger@gmail.com>
2019-09-23 18:37:37 -0600

    assigned function calls to variables

--- 001/utility_functions/main.cpp
+++ 001/utility_functions/main.cpp
@@ -1,10 +1,15 @@
 #include "UtilityFunctions.h"
+#include <iostream>
 
 
 int main() {
     vector<int> add5 = {0, 5, -5};
     vector<int> addmin5 = {0, 5, -5};
     vector<int> add99 = {0, 5, -5};
+
+    add5 = VectorPlusN(add5, 5);
+    addmin5 = VectorPlusN(addmin5, -5);
+    add99 = VectorPlusN(add99, 99);
 
     for (unsigned int i = 0; i < add5.size(); i++){
         cout<<add5[i]<<", ";
