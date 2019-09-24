a354e0743510b76ac9ffc72cd4d382ccf9bdc6e9
LucasKolanz <luko8622@colorado.edu>
2019-09-23 19:46:09 -0600

    Added special cases

--- 001/utility_functions/UtilityFunctions.cpp
+++ 001/utility_functions/UtilityFunctions.cpp
@@ -4,6 +4,14 @@
 
 int Factorial(int n)
 {
+	if (n == 0)
+	{
+		return 1;
+	}
+	else if (n == 1)
+	{
+		return 1;
+	}
 	int returnThis = 1;
 	for (int i = 1; i <= n; i++)
 	{
--- 001/utility_functions/main.cpp
+++ 001/utility_functions/main.cpp
@@ -1,10 +1,15 @@
 #include "UtilityFunctions.h"
 
 
-int main() {
+int main() 
+{
     // the main function should have demonstrations
     // of running your functions.
     // make sure that the output is easily interpretable.
     // If you'd like to define helper functions in this file,
     // you are welcome to do so!
+
+	int factorial_this = 4;
+    std::cout<<"This is our factorial function!"<<std::endl;
+    std::cout<<"It takes an int such as ("<<<<std::endl;
 }
