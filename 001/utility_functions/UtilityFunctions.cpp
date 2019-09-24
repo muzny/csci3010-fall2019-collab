5b12e263b3289f07f522f9eb9953b76cccbdf939
Innamorat0 <sobe4920@colorado.edu>
2019-09-24 01:12:34 +0000

    s

--- 001/utility_functions/UtilityFunctions.cpp
+++ 001/utility_functions/UtilityFunctions.cpp
@@ -1,3 +1,17 @@
 #include "UtilityFunctions.h"
-
+#include <vector>
 // Implement your functions here. Don't forget function comments!
+
+std::vector<bool>  EvenMask(std::vector<int>V){
+	std::vector <bool> temp;
+for (int i=0; i<V.size(); i++){
+	if (V[i]%2==0){
+		temp.push_back(true);	
+	}
+	else {
+		temp.push_back(false);
+	}
+}
+return temp; 
+
+}
--- 001/utility_functions/test.cpp
+++ 001/utility_functions/test.cpp
@@ -1,8 +1,26 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include <iostream>
+using namespace std; 
+#include <vector>
 
 #include "UtilityFunctions.h"
 
 // Your tests go here
 // Each TEST_CASE should test one function
 // Each SECTION should test one aspect of that function
+
+TEST_CASE("Testing odd or even", "[EvenMask]"){
+	SECTION ("Testing a combination"){
+		vector<int> check1 = {1,2,3,4,5,6,7,8,9};
+		vector<bool> temp = {0,1,0,1,0,1,0,1,0};
+		REQUIRE(EvenMask(check1)== temp);
+	}
+	
+	SECTION ("Testing all even"){
+		vector<int> check2 = {4,6,8,10};
+		vector<bool> temp = {1,1,1,1};
+		REQUIRE(EvenMask(check2)==temp);
+	}
+	
+}
