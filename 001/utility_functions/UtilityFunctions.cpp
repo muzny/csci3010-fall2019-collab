38785aa9613a68f66a2a7992576b907258895106
sako0518 <46761406+sako0518@users.noreply.github.com>
2019-09-25 00:31:57 -0600

    Added function functionality

--- 001/utility_functions/UtilityFunctions.cpp
+++ 001/utility_functions/UtilityFunctions.cpp
@@ -3,4 +3,12 @@
 // Implement your functions here. Don't forget function comments!
 // takes two vectors of integers, a and b. The function then removes elements from a if they are also in b.
 // If the integer is in b, but not in a, nothing happens.
-std::vector<int> MatchVectors(std::vector<int> a, std::vector<int> b);
+std::vector<int> MatchVectors(std::vector<int> a, std::vector<int> b){
+	std::vector<int> ret = b;
+	for (int i; i < b.size(); i++){
+		for (int j; j < a.size(); j++){
+			if (b[i] == a[j])
+				a.erase(vec.begin() + j);
+		}		
+	}
+};
