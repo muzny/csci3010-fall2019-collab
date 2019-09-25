8c5e97a91b59844cc615c3647ec8749c6b074e5b
limc6048 <43150982+limc6048@users.noreply.github.com>
2019-09-25 00:20:08 -0600

    Added implementation of TakeInventory function

--- library/run_library.cpp
+++ library/run_library.cpp
@@ -12,5 +12,17 @@ int main() {
 
     // start off with one library instantiated
     Library lib;
-
+    Book a;
+    Book b;
+    Book c;
+    a.title = "Heart of Darkness";
+    a.author = "Joseph Conrad";
+    b.title = "The Catcher and the Rye";
+    b.author = "J.D. Salinger";
+    c.title = "The Great Gatsby";
+    c.author = "F. Scott Fitzgerald";
+    lib.Donate(a);
+    lib.Donate(b);
+    lib.Donate(c);
+    lib.TakeInventory();
 }
