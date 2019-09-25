b262ee2a81dc7f97275a2aebf2384c8d10a24aff
sako0518 <46761406+sako0518@users.noreply.github.com>
2019-09-25 02:48:10 -0600

    added #include vector

--- 001/utility_functions/UtilityFunctions.h
+++ 001/utility_functions/UtilityFunctions.h
@@ -2,4 +2,5 @@
 // they should not be in a class.
 // takes two vectors of integers, a and b. The function then removes elements from a if they are also in b.
 // If the integer is in b, but not in a, nothing happens.
+#include <vector>
 std::vector<int> MatchVectors(std::vector<int> a, std::vector<int> b);
