6d629ce4873b3ecef1a8b919e3888796e8fb2cf1
apkedm <deanneapke@gmail.com>
2019-09-25 08:47:40 -0600

    Added in VectorTimesN and tests for VectorTimesN

--- 001/utility_functions/UtilityFunctions.cpp
+++ 001/utility_functions/UtilityFunctions.cpp
@@ -1,3 +1,18 @@
 #include "UtilityFunctions.h"
 
 // Implement your functions here. Don't forget function comments!
+
+/**
+    Multiplies each entry within a given vector by a given integer and saves
+    the new values to a new vector to return
+
+    @param a vector of integers and the integer to multiply each entry in the vector by
+    @return a vector with the resulting values
+*/
+std::vector<int> VectorTimesN(std::vector<int> v, int n) {
+  std::vector<int> return_vec;
+  for(int i = 0; i < v.size(); i++) {
+    return_vec.push_back(v[i] * n);
+  }
+  return return_vec;
+}
--- 001/utility_functions/UtilityFunctions.h
+++ 001/utility_functions/UtilityFunctions.h
@@ -1,2 +1,5 @@
 // put the declarations of your utility functions here.
 // they should not be in a class.
+#include <vector>
+
+std::vector<int> VectorTimesN(std::vector<int> v, int n);
