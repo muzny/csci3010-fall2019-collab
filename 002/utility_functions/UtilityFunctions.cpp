ce0c49a42cba9c4187bc1306cd3308f1b07001e2
Nasuru <nasuruf@gmail.com>
2019-09-24 19:40:19 -0600

    Added split function

--- 002/utility_functions/UtilityFunctions.cpp
+++ 002/utility_functions/UtilityFunctions.cpp
@@ -1,3 +1,29 @@
 #include "UtilityFunctions.h"
 
 // Implement your functions here. Don't forget function comments!
+
+#include "UtilityFunctions.h"
+#include <vector>
+#include <string>
+
+// Splits a single string on separator into a vector of strings
+std::vector<std::string> Split(std::string whole, std::string separator){
+    std::vector<std::string> ret;
+    int prev_sep = -1;
+    int curr_sep = -1;
+    for(int i=0;i<whole.size();i++){
+        if(whole[i] == separator){
+            curr_sep = i;
+            int sub_len = curr_sep-prev_sep-1;
+            if(sub_len > 0){
+                ret.push_back(whole.substr(prev_sep+1,sub_len));
+            }
+            prev_sep = curr_sep;
+        }
+    }
+    int rem_len = whole.size()-prev_sep-1;
+    if(rem_len > 0){
+        ret.push_back(whole.substr(prev_sep+1,rem_len));
+    }
+    return ret;
+}
--- 002/utility_functions/UtilityFunctions.h
+++ 002/utility_functions/UtilityFunctions.h
@@ -1,2 +1,4 @@
 // put the declarations of your utility functions here.
 // they should not be in a class.
+
+std::vector<std::string> Split(std::string s1, std::string s2); 
