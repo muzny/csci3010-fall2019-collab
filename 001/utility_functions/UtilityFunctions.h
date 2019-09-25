154b56d17c24a8cbc0579538c30bd25548dd0ec2
sako0518 <46761406+sako0518@users.noreply.github.com>
2019-09-25 00:31:26 -0600

    added header function MatchVectors

--- 001/utility_functions/UtilityFunctions.h
+++ 001/utility_functions/UtilityFunctions.h
@@ -1,2 +1,5 @@
 // put the declarations of your utility functions here.
 // they should not be in a class.
+// takes two vectors of integers, a and b. The function then removes elements from a if they are also in b.
+// If the integer is in b, but not in a, nothing happens.
+std::vector<int> MatchVectors(std::vector<int> a, std::vector<int> b);
