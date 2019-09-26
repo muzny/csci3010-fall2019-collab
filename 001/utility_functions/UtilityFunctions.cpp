9a58512ec89b0495ac32f3928f3e7f269a37413f
RadArcadeKid <jakegatsby935@gmail.com>
2019-09-26 13:17:09 -0600

    Updated function comments

--- 001/utility_functions/UtilityFunctions.cpp
+++ 001/utility_functions/UtilityFunctions.cpp
@@ -4,6 +4,8 @@
 
 /**
   SubtractN - Subtracts the value of the parameter int n from every element in vector v
+  @vector v - a vector that is being passed-in, @int n is the value to subtract from each element
+  @returns a modified vector v 
 */
 std::vector<int> SubtractN(std::vector<int> v, int n){
     for(unsigned int i = 0; i < v.size(); i++){
