85a22f6e49b92c892846b13c7fd2e9680de37968
coryflynn <coryjamesflynn@hotmail.com>
2019-09-27 03:14:42 +0000

    c1

--- 002/utility_functions/test.cpp
+++ 002/utility_functions/test.cpp
@@ -1,8 +1,35 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
+
 #include "catch.hpp"
+#include <iostream>
+#include <vector>
 
-#include "UtilityFunctions.h"
+using namespace std;
 
-// Your tests go here
-// Each TEST_CASE should test one function
-// Each SECTION should test one aspect of that function
+string Join(vector<string> pieces, string glue) {
+  string s;
+  for (int i = 0; i < pieces.size(); i++){
+    if(i < pieces.size() - 1)
+      s = s + pieces[i] + glue;
+    else
+      s = s + pieces[i];
+  };
+  return s;
+}
+
+TEST_CASE( "combines vector to string with glue in between", "[vector]" ) {
+  std::vector<string> a{"wow"};
+  string glue = "-";
+  SECTION("vector of single entity", "[vector]"){
+    REQUIRE( Join(a, glue) == "wow");
+  };
+  vector<string> b{"wow", "this", "is", "COOOOOL"};
+  SECTION("vector of multiple entities", "[vector]"){
+    REQUIRE( Join(b, glue) == "wow-this-is-COOOOOL");
+    glue =  " ";
+    REQUIRE( Join(b, glue) == "wow this is COOOOOL");
+  }
+}
+
+
+
