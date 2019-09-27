cbdddda108871bf901dd8be94b01a071fa2def95
willculkin <43355842+willculkin@users.noreply.github.com>
2019-09-27 12:16:08 -0600

    lec8

--- 002/utility_functions/UtilityFunctions.h
+++ 002/utility_functions/UtilityFunctions.h
@@ -1,2 +1,7 @@
+#include <iostream>
+#include <vector>
+std::vector<int> VectorPlusN(std::vector<int> v, int n);
+
+
 // put the declarations of your utility functions here.
 // they should not be in a class.
