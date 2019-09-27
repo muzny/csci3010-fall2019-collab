b6ad01cfde712270542d415899c5e658d5a46e0c
willculkin <43355842+willculkin@users.noreply.github.com>
2019-09-27 12:14:37 -0600

    update for lec8
    vector addN

--- 002/utility_functions/UtilityFunctions.cpp
+++ 002/utility_functions/UtilityFunctions.cpp
@@ -1,3 +1,12 @@
 #include "UtilityFunctions.h"
 
+
 // Implement your functions here. Don't forget function comments!
+
+std::vector<int> VectorPlusN(std::vector<int> v, int n){
+	std::vector<int> newV;
+	for(int i = 0; i < v.size(); i++){
+		newV .push_back(v[i] + n);
+	}
+	return newV;
+}
