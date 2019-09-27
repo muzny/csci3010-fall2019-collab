855a410b5d8b3faa062bdb74a7780a3794a4c876
willculkin <43355842+willculkin@users.noreply.github.com>
2019-09-27 12:16:51 -0600

    lec8

--- 002/utility_functions/main.cpp
+++ 002/utility_functions/main.cpp
@@ -1,5 +1,5 @@
 #include "UtilityFunctions.h"
-
+using namespace std;
 
 int main() {
     // the main function should have demonstrations
@@ -7,4 +7,11 @@ int main() {
     // make sure that the output is easily interpretable.
     // If you'd like to define helper functions in this file,
     // you are welcome to do so!
+	vector<int> v{ 0 };
+    vector<int> des{ 1 };
+
+    vector<int> res = VectorPlusN( v, 1 );
+
+    bool matches = res == des;
+    cout << matches << endl;
 }
