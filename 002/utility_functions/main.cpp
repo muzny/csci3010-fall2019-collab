41fea0a40c37431522628f3e7a5935195e5f082a
cocraighead <colincraighead@yahoo.com>
2019-09-29 01:14:38 +0000

    Added a test into main

--- 002/utility_functions/main.cpp
+++ 002/utility_functions/main.cpp
@@ -1,6 +1,10 @@
 #include "UtilityFunctions.h"
 #include "catch.hpp"
+#include <string>
+#include <iostream>
 
 int main(){
+    std::string str = "Hi this is Colin. Colin says Hi";
+    std::cout << RemoveFirstSubstring(str, "Colin") << std::endl;
     return 0;
 }
