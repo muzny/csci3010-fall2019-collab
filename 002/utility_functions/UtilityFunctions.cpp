1fd5d24e86c643fcb2bdca49c0c2389b56e5cfaa
sbkeupp <43832329+sbkeupp@users.noreply.github.com>
2019-10-01 13:02:35 -0600

    Update UtilityFunctions.cpp

--- 002/utility_functions/UtilityFunctions.cpp
+++ 002/utility_functions/UtilityFunctions.cpp
@@ -24,17 +24,3 @@ vector<bool> GreaterMask(vector<int> nums, int greater_than){
 }
 
 // Implement your functions here. Don't forget function comments!
-
-// subtracts n to each element of the vector
-vector<int> SubtractN(vector<int> vec, int n)
-{
-    //Loops through the vector
-    for (int x = 0; x < vec.size(); x++)
-    {
-        //subtracts n from each index in the vector
-        vec[x] = vec[x] - n;
-    }
-
-    //returns the altered vector
-    return vec;
-}
