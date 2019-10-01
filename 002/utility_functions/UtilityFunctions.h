f8b9cc2f3acde0e151cd4dc0b6c466c552e328e5
sbkeupp <43832329+sbkeupp@users.noreply.github.com>
2019-10-01 13:03:03 -0600

    Update UtilityFunctions.h

--- 002/utility_functions/UtilityFunctions.h
+++ 002/utility_functions/UtilityFunctions.h
@@ -1,9 +1,6 @@
 #ifndef UTILITYFUNCTIONS_H
 #define UTILITYFUNCTIONS_H
 
-#include <vector>
-using namespace std;
-
 // put the declarations of your utility functions here.
 // they should not be in a class.
 #include <iostream>
@@ -11,8 +8,5 @@ using namespace std;
 using namespace std;
 
 vector<bool> GreaterMask(vector<int> nums, int greater_than);
-
-// subtracts n to each element of the vector
-vector<int> SubtractN(vector<int> vec, int n);
 
 #endif
