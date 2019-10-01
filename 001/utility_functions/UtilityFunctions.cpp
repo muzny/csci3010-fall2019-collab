3e325ada11f5c3d2104e3e9425c1658d1b67c7a6
Oceane Andreis <ocan3794@colorado.edu>
2019-09-30 20:45:36 -0600

    Updated UtilityFunctions.cpp
    pe6

--- 001/utility_functions/UtilityFunctions.cpp
+++ 001/utility_functions/UtilityFunctions.cpp
@@ -1,12 +1,17 @@
 #include "UtilityFunctions.h"
 
-// Implement your functions here. Don't forget function comments!
+/**
+Oceane Andreis & Mikayla Pickett
+
+This is our utility function file. I have a function that take in a vector of ints and the function goes through
+all the elements in the vector and adds them all up and it returns the sum of the vector.
+*/
 
 int Sum(vector<int> nums){
      
     int sum = 0;
     
-    for(unsigned int i =0; i<nums.size(); i++)
+    for(unsigned int i = 0; i < nums.size(); i++)
     {
         sum+=nums[i];
     }
