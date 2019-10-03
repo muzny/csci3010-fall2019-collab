8a2449f0c03c5530003341d463645ba6cb5acb31
Trevor-J <trje9656@colorado.edu>
2019-10-02 22:34:36 -0600

    added comments

--- 002/utility_functions/UtilityFunctions.cpp
+++ 002/utility_functions/UtilityFunctions.cpp
@@ -7,10 +7,12 @@
 std::vector<bool> OddMask(std::vector<int>numbers)
 {
 
+//store the result of the check for even or odd, true means odd
 std::vector<bool>odd_or_even;
-
+//iterate through the provided vector of integers
     for(auto&& x: numbers)
     {
+        //add true if a number is odd, false otherwise
         if((x%2)==1)
         {
             odd_or_even.push_back(true);
@@ -22,6 +24,6 @@ std::vector<bool>odd_or_even;
         
     }
 
-
+//return the vector after checking all numbers
 return odd_or_even;
 }
