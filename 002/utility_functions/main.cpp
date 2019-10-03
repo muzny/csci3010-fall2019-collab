c8ac8f656c609f1024e1e501f2fafc5fea59e086
Trevor Jedziniak <32111962+trje9656@users.noreply.github.com>
2019-10-03 10:56:07 -0600

    Update main.cpp
    a merge from master seemed to delete code for some reason

--- 002/utility_functions/main.cpp
+++ 002/utility_functions/main.cpp
@@ -19,7 +19,9 @@ int main() {
     print(OddMask(vect));
     vector<int> test_vector = {1, 2, 3, 4};
     vector<bool> output_vector;
+	
     output_vector = GreaterMask(test_vector, 3);
+    for(int i=0; i < output_vector.size(); i++){
         cout << output_vector[i] << endl;
     }
     
