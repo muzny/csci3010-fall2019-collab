c74ddb29811bc958eb2aa885653e7b9927d5a8b5
coryflynn <coryjamesflynn@hotmail.com>
2019-10-03 21:35:39 +0000

    more issues with merging

--- 002/utility_functions/main.cpp
+++ 002/utility_functions/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "UtilityFunctions.cpp"
 #include <iostream>
 #include <vector>
@@ -11,36 +10,3 @@ int main() {
     cout << Join(my_fav, glue) << '\n';
     //Prints string "I Like PB&J"
 }
-=======
-#include "UtilityFunctions.h"
-#include "catch.hpp"
-#include <string>
-#include <iostream>
-#include <vector>
-using namespace std;
-//https://www.techiedelight.com/print-vector-cpp/
-void print(std::vector<bool> const &input)
-{
-    std::cout <<"A 1 means odd number, a 0 means an even number."<< std::endl;
-	for (int i = 0; i < input.size(); i++) {
-		std::cout << input.at(i) << ' ';
-	}
-    std::cout << std::endl;
-}
-
-int main() {
-    std::vector<int>vect{ 10, 3, 4, 5, 7, 20, 30 };
-    print(OddMask(vect));
-    vector<int> test_vector = {1, 2, 3, 4};
-    vector<bool> output_vector;
-	
-    output_vector = GreaterMask(test_vector, 3);
-    for(int i=0; i < output_vector.size(); i++){
-        cout << output_vector[i] << endl;
-    }
-    
-    string str = "Hi this is Colin. Colin says Hi";
-    cout << RemoveFirstSubstring(str, "Colin") << endl;
-}
-
->>>>>>> c52f368b7e50b51206528178327a87d2c1c29d72
