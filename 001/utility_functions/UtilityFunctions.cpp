639e75c2f79554dc07269ca8c8c6c38765a7200b
Zoolouie <allo1877@colorado.edu>
2019-10-03 14:00:02 -0600

    Added function comment

--- 001/utility_functions/UtilityFunctions.cpp
+++ 001/utility_functions/UtilityFunctions.cpp
@@ -1,8 +1,13 @@
 #include "UtilityFunctions.h"
-#include <vector>
+#include <vector> //Added vector for easier array-type access.
 #include<stdio.h> 
 
 // Implement your functions here. Don't forget function comments!
+
+/* Function: VectorPlusN
+   Parameters: A vector (v) to add a double (n) to each element
+   Returns: A new vector updated with each of the numbers
+*/
 
 std::vector<double> VectorPlusN(std::vector<double> v, double n) {
 	std::vector<double> plus_n;
