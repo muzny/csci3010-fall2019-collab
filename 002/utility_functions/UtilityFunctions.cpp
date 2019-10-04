56ae3b82199eb7a7cc4e7a049ce63f6191547e15
kri111 <krishna.adettiwar@colorado.edu>
2019-10-04 10:28:21 -0600

    Update UtilityFunctions.cpp

--- 002/utility_functions/UtilityFunctions.cpp
+++ 002/utility_functions/UtilityFunctions.cpp
@@ -1,19 +1,6 @@
-#include <vector>
 #include "UtilityFunctions.h"
-#include <string>
 #include <iostream>
 using namespace std;
-
-string Join(vector<string> pieces, string glue) {
-  string s;
-  for (int unsigned i = 0; i < pieces.size(); i++){
-    if(i < pieces.size() - 1)
-      s = s + pieces[i] + glue;
-    else
-      s = s + pieces[i];
-  };
-  return s;
-}
 
 // checks the sign of num. returns -1 if negative, 1 if positive.
 double Sign(double num){
