81e77da384a119616ee9c14dbc46a416bd4686bf
kri111 <krishna.adettiwar@colorado.edu>
2019-10-04 10:31:29 -0600

    Update test.cpp

--- 002/utility_functions/test.cpp
+++ 002/utility_functions/test.cpp
@@ -3,35 +3,11 @@
 
 #include "catch.hpp"
 #include <iostream>
-#include <vector>
 
 using namespace std;
-
-TEST_CASE ( "Vector is working.") {
-    
-    REQUIRE( GreaterMask(test_vector, greater_than) ==  correct_vector);
-    REQUIRE( GreaterMask(test_vector2, greater_than2) == correct_vector2 );
-}
-// Your tests go here
-// Each TEST_CASE should test one function
-// Each SECTION should test one aspect of that function
-
 
 // tests the sign function with two sections (checks both positive and negative cases)
 TEST_CASE() {
     REQUIRE( Sign(4.5) == 1 );
     REQUIRE( Sign(-2.764) == -1 );
-}
-  TEST_CASE( "combines vector to string with glue in between", "[vector]" ) {
-  std::vector<string> a{"wow"};
-  string glue = "-";
-  SECTION("vector of single entity", "[vector]"){
-    REQUIRE( Join(a, glue) == "wow");
-  };
-  vector<string> b{"wow", "this", "is", "COOOOOL"};
-  SECTION("vector of multiple entities", "[vector]"){
-    REQUIRE( Join(b, glue) == "wow-this-is-COOOOOL");
-    glue =  " ";
-    REQUIRE( Join(b, glue) == "wow this is COOOOOL");
-  }
 }
