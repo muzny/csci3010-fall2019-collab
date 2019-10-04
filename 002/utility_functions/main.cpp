0933f898ae66fdab0e6aa2a17df5fc16c2e517c7
kri111 <krishna.adettiwar@colorado.edu>
2019-10-03 20:27:43 -0600

    Update main.cpp

--- 002/utility_functions/main.cpp
+++ 002/utility_functions/main.cpp
@@ -1,6 +1,7 @@
 #include "UtilityFunctions.h"
 #include <iostream>
-
+#include <vector>
+using namespace std;
 
 int main() {
     // the main function should have demonstrations
@@ -9,16 +10,10 @@ int main() {
     // If you'd like to define helper functions in this file,
     // you are welcome to do so!
 
-
-#include <vector>
-
-using namespace std;
-
-int main() {
-
     vector<int> test_vector = {1, 2, 3, 4};
     vector<bool> output_vector;
     output_vector = GreaterMask(test_vector, 3);
+    
     for(int i=0; i < output_vector.size(); i++){
         cout << output_vector[i] << endl;
     }
