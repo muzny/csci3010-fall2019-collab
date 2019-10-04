ea1fb244910dbe2f71114c29eb0839b6dc4f0b93
kri111 <krishna.adettiwar@colorado.edu>
2019-10-04 10:29:08 -0600

    Update main.cpp

--- 002/utility_functions/main.cpp
+++ 002/utility_functions/main.cpp
@@ -10,20 +10,7 @@ int main() {
     // If you'd like to define helper functions in this file,
     // you are welcome to do so!
 
-    vector<int> test_vector = {1, 2, 3, 4};
-    vector<bool> output_vector;
-    output_vector = GreaterMask(test_vector, 3);
-    
-    for(int i=0; i < output_vector.size(); i++){
-        cout << output_vector[i] << endl;
-    }
-  
     // these two cout statements show the sign function is working. prints 1 for positive numbers, and -1 for negative numbers.
     std::cout << Sign(5.4) << std::endl;
     std::cout << Sign(-2.2) << std::endl;
-
-    std::vector<string> my_fav = {"I", "Like", "PB&J"};
-    string glue = " ";
-    cout << Join(my_fav, glue) << endl;
-    //Prints string "I Like PB&J"
 }
