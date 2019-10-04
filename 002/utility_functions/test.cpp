e134cc897fa81b3f5339de0e1a73d60fd93c4a13
outdrzyflrzy <55072491+outdrzyflrzy@users.noreply.github.com>
2019-10-04 10:56:35 -0600

    Update test.cpp

--- 002/utility_functions/test.cpp
+++ 002/utility_functions/test.cpp
@@ -33,7 +33,7 @@ TEST_CASE( "combines vector to string with glue in between", "[vector]" ) {
   string glue = "-";
   SECTION("vector of single entity", "[vector]"){
     REQUIRE( Join(a, glue) == "wow");
-  };
+  }
   vector<string> b{"wow", "this", "is", "COOOOOL"};
   SECTION("vector of multiple entities", "[vector]"){
     REQUIRE( Join(b, glue) == "wow-this-is-COOOOOL");
