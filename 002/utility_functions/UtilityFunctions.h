f47d6416d725d68f30e998b1c7f3000898654d5f
Chris Kardaras <ckardaras92@gmail.com>
2019-10-04 10:03:07 -0700

    fix

--- 002/utility_functions/UtilityFunctions.h
+++ 002/utility_functions/UtilityFunctions.h
@@ -1,15 +1,17 @@
 // put the declarations of your utility functions here.
 // they should not be in a class.
 //Returns the factorial of n. 
-int Factorial(int n);
 #include <vector>
 #include <iostream>
 using namespace std;
 
-int Sign (int num);
+int Factorial(int n);
 
+int Sign (int num);
 
 // subtracts n to each element of the vector
 vector<double> SubtractN(vector<double> input, double n);
+
+string RemoveFirstSubstring(string s1, string s2);
 
 #endif
