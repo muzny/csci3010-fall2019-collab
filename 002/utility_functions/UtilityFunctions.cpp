561765eb29202f877df6d85fa1ddb0f8ccb315fe
Chris Kardaras <ckardaras92@gmail.com>
2019-10-04 09:56:06 -0700

    fixed error

--- 002/utility_functions/UtilityFunctions.cpp
+++ 002/utility_functions/UtilityFunctions.cpp
@@ -49,10 +49,10 @@ vector<double> SubtractN(vector<double> input, double n)
 }
 
 // takes two strings and returns a new string that is the result of removing the first occurrence of s2 from s1.
-string RemoveFirstSubstring(string s1, string s2){
+string RemoveFirstSubstring(string s1, string s2)
+{
     int first_index = s1.find(s2); //First index of the instace of the sub string
     if(first_index < 0){ // if there isn't an instance do nothing
         return s1;
-    }
     return s1.substr(0,first_index) + s1.substr(first_index+s2.size(),-1); // return the substring before and after the instance
 }
