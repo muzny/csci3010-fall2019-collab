56d5b60cb0522d898af88df77b67ff3e8484a289
kri111 <krishna.adettiwar@colorado.edu>
2019-10-04 10:28:43 -0600

    Update UtilityFunctions.h

--- 002/utility_functions/UtilityFunctions.h
+++ 002/utility_functions/UtilityFunctions.h
@@ -4,13 +4,10 @@
 // put the declarations of your utility functions here.
 // they should not be in a class.
 
-#include <vector>
 #include <iostream>
 using namespace std;
 
 // checks the sign of num. returns -1 if negative, 1 if positive.
 double Sign(double num);
-vector<bool> GreaterMask(vector<int> nums, int greater_than);
-string Join(std::vector<string> pieces, string glue);
 
 #endif
