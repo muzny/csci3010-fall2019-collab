996838a32e50cd0347e6b78791b7092e7b3d26e1
abna2071 <35585395+abna2071@users.noreply.github.com>
2019-10-03 22:08:00 -0600

    Update UtilityFunctions.h

--- 002/utility_functions/UtilityFunctions.h
+++ 002/utility_functions/UtilityFunctions.h
@@ -3,4 +3,5 @@
 #include <iostream> //input and output stream
 #include <vector> //ability to use vectors
 
+//Takes a vector of integers and returns the sum of the integers within the vectors
 int Sum(std::vector<int> nums);
