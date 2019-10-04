6db0f4c7ccf93fe74ce9fb7c5382dc24cfe5251a
Chris Kardaras <ckardaras92@gmail.com>
2019-10-04 09:59:57 -0700

    fixes

--- 002/utility_functions/main.cpp
+++ 002/utility_functions/main.cpp
@@ -6,14 +6,8 @@
 using namespace std;
 
 
-int main() {
-    vector<int> test_vector = {1, 2, 3, 4};
-    vector<bool> output_vector;
-    output_vector = GreaterMask(test_vector, 3);
-    for(int i=0; i < output_vector.size(); i++)
-    {
-        cout << output_vector[i] << endl;
-    }
+int main() 
+{
 
     vector<double> someVector={2,4,7,-3,0};
     vector<double> result = SubtractN(someVector,2);
