160a50aa7f11f9259d4b7ab4258524f6744969d0
limc6048 <43150982+limc6048@users.noreply.github.com>
2019-10-04 12:41:53 -0600

    Update UtilityFunctions.h

--- 002/utility_functions/UtilityFunctions.h
+++ 002/utility_functions/UtilityFunctions.h
@@ -1,55 +1,5 @@
-
-#ifndef _UTILITY_FUNCTIONS_H_
-#define _UTILITY_FUNCTIONS_H_
-
-
 // put the declarations of your utility functions here.
 // they should not be in a class.
-
 #include <vector>
 
 double Sum(std::vector<double> nums);
-
-//Returns the factorial of n. 
-int Factorial(int n);
-
-#ifndef _UTILITYFUNCTIONS_H_
-#define _UTILITYFUNCTIONS_H_
-
-#include <vector>
-#include <iostream>
-
-
-// put the declarations of your utility functions here.
-// they should not be in a class.
-
-//Returns the factorial of n. 
-int Factorial(int n);
-
-
-
-
-
-std::vector<int> VectorTimesN(std::vector<int> v, int n);
-
-vector<bool> GreaterMask(vector<int> nums, int greater_than);
-
-string Join(std::vector<string> pieces, string glue);
-int Sign (int num);
-
-
-// checks the sign of num. returns -1 if negative, 1 if positive.
-double Sign(double num);
-
-// adds n to each element of the vector
-std::vector<std::string> AddN(std::vector<std::string> strvect, std::string n);
-
-// multiplies all the vector stuff
-int Product(vector<int> nums);
-
-
-int fibonacci(int n);
-
-
-#endif // UTILITY_FUNCTIONS
-
