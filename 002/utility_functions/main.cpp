fbfa408f1f9e54b7477faedaeedf372d1e51cd2f
limc6048 <43150982+limc6048@users.noreply.github.com>
2019-10-04 12:42:38 -0600

    Update main.cpp

--- 002/utility_functions/main.cpp
+++ 002/utility_functions/main.cpp
@@ -1,23 +1,14 @@
 #include "UtilityFunctions.h"
-
+#include <iostream>
 #include <vector>
 
-#include <iostream>
 
-
-using namespace std;
-
-
-int main()
-{
+int main() {
     // the main function should have demonstrations
     // of running your functions.
     // make sure that the output is easily interpretable.
     // If you'd like to define helper functions in this file,
     // you are welcome to do so!
-    std::cout << fibonacci(15) << '\n';
-
-
 
     std::vector<double> v1{18.1, 1.3, 0.7};
     std::vector<double> v2{0.1, 0.4, 0.9, 0.5};
@@ -28,35 +19,5 @@ int main()
     std::cout << "The sum of v3 is " << Sum(v3) << std::endl;
 
     return 1;
-  
-
-  
-    // these two cout statements show the sign function is working. prints 1 for positive numbers, and -1 for negative numbers.
-    cout << Sign(5.4) << endl;
-    cout << Sign(-2.2) << endl;
-
-
-    int a=2;
-    int Sign(a);
-   // int Sign (-3);
-
-
-
-    //Kevin Ruby -- A program to multiple all elements in vector by n.
-
-	vector<int> input{3,4,5};
-    vector<int> output = VectorTimesN(input,5);
-
-    for (int i=0;i<output.size();i++){
-    	cout << output[i] <<"  ";
-    }
-    cout<<endl;
-    return 0;
-
-    std::vector<string> my_fav = {"I", "Like", "PB&J"};
-    string glue = " ";
-    cout << Join(my_fav, glue) << endl;
-    //Prints string "I Like PB&J"
-
-
 }
+
